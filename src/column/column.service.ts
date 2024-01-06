00f87e9e90f648bf3b307866409848305bc2c26c
gyusungjung <wjdrbtjd92@gmail.com>
2024-01-06 12:28:59 +0900

    일단 올림

--- src/column/column.service.ts
+++ src/column/column.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ColumnModel } from './entities/column.entity';
+import { CreateColumnDto } from './dtos/create-column.dto';
+import { UpdateColumnDto } from './dtos/update-column.dto';
 
 @Injectable()
-export class ColumnService {}
+export class ColumnService {
+  constructor(
+    @InjectRepository(ColumnModel)
+    private columnRepository: Repository<ColumnModel>,
+  ) {}
+
+  async createColumn(
+    boardId: number,
+    createColumnDto: CreateColumnDto,
+  ): Promise<ColumnModel> {
+    const lastColumn = await this.columnRepository.find({
+      where: { boardId },
+      order: { order: 'DESC' },
+      take: 1,
+    });
+    const order = lastColumn.length > 0 ? lastColumn[0].order + 1 : 1;
+
+    const newColumn = this.columnRepository.create({
+      ...createColumnDto,
+      boardId,
+      order,
+    });
+    return this.columnRepository.save(newColumn);
+  }
+
+  async updateColumn(
+    id: number,
+    updateColumnDto: UpdateColumnDto,
+  ): Promise<ColumnModel> {
+    await this.columnRepository.update(id, updateColumnDto);
+    return this.columnRepository.findOne({ where: { id } });
+  }
+
+  async deleteColumn(id: number): Promise<void> {
+    await this.columnRepository.delete(id);
+  }
+}
