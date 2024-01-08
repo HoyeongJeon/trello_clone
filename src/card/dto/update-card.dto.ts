f734e64aecd1826b86e4bdc4cb2c35d74cc519d2
unknown <rladydrms98@naver.com>
2024-01-08 15:34:59 +0900

    feat : [card-detail/date] 카드 시작,마감일 기능

--- src/card/dto/update-card.dto.ts
+++ src/card/dto/update-card.dto.ts
@@ -1,9 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CardModel } from '../entities/card.entity';
+import { IsDateString, IsOptional } from 'class-validator';
 
 export class UpdateCardDto extends PartialType(CardModel) {
   title?: string;
   members?: string;
   description?: string;
   color?: string;
+  @IsDateString()
+  @IsOptional()
+  startDate?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  dueDate?: Date;
 }
