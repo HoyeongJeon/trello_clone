5a55af9c993a03f347911785fe0b265ccb9ac324
nohsiwon <98876@naver.com>
2024-01-08 20:00:49 +0900

    add boardId/findAll

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -3,9 +3,10 @@ import { CardService } from './card.service';
 import { CardController } from './card.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardModel } from './entities/card.entity';
+import { ColumnModel } from 'src/column/entities/column.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CardModel])],
+  imports: [TypeOrmModule.forFeature([CardModel, ColumnModel])],
   controllers: [CardController],
   providers: [CardService],
 })
