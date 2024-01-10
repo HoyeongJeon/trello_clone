f2ae40816f42966caf102a2ef7e3beff42a0ad23
HoyeongJeon <hoyeong0302@gmail.com>
2024-01-10 20:08:34 +0900

    feat: 컬럼 권한 체크

--- src/column/column.module.ts
+++ src/column/column.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ColumnService } from './column.service';
 import { ColumnController } from './column.controller';
 import { BoardModel } from 'src/board/entities/board.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColumnModel } from './entities/column.entity';
 import { CardModule } from 'src/card/card.module';
+import { BoardModule } from 'src/board/board.module';
 
 @Module({
   exports: [ColumnService],
-  imports: [TypeOrmModule.forFeature([BoardModel, ColumnModel]), CardModule],
+  imports: [
+    TypeOrmModule.forFeature([BoardModel, ColumnModel]),
+    CardModule,
+    forwardRef(() => BoardModule),
+  ],
   controllers: [ColumnController],
   providers: [ColumnService],
 })
